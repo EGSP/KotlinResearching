cac35659a7b2bcedf609f05083fc36693250e2a3
EveryGameSPlay <repsbro@gmail.com>
2022-09-06 16:13:45 +0300

    Added number token scanner function

--- src/main/kotlin/interpreter/Scanner.kt
+++ src/main/kotlin/interpreter/Scanner.kt
@@ -2,6 +2,7 @@ package interpreter
 
 import interpreter.TokenType.*
 import java.util.*
+import kotlin.math.absoluteValue
 
 
 class Scanner(private val source:String, private val interpreter: Interpreter) {
@@ -10,6 +11,10 @@ class Scanner(private val source:String, private val interpreter: Interpreter) {
     private var entry:Int = 0
     private var pointer:Int = 0
     private var line:Int = 1
+
+    private val current get() = getPointedChar()
+    private val next get() = getNextChar()
+
 
     fun scanTokens():List<Token>{
         if(source.isEmpty())
@@ -25,7 +30,7 @@ class Scanner(private val source:String, private val interpreter: Interpreter) {
     }
 
     private fun scanToken(){
-        val character = getPointedChar()
+        val character = current
 
         when (character) {
             '(' -> addToken(LEFT_PAREN)
@@ -66,8 +71,6 @@ class Scanner(private val source:String, private val interpreter: Interpreter) {
                 }
             }
 
-            '"' -> addString()
-
             // Игнорируем пробелы
             ' ' -> return
             '\r'-> return
@@ -75,7 +78,15 @@ class Scanner(private val source:String, private val interpreter: Interpreter) {
 
             '\n'-> incrementLine()
 
-            else -> Interpreter.error(interpreter,line,"Unexpected character [$character]")
+            '"' -> addString()
+
+            else -> {
+                if(character.isDigit()){
+                    addNumber()
+                }else {
+                    Interpreter.error(interpreter, line, "Unexpected character [$character]")
+                }
+            }
         }
         movePointer()
     }
@@ -84,15 +95,31 @@ class Scanner(private val source:String, private val interpreter: Interpreter) {
         return pointer >= source.length
     }
 
-    private fun getPointedChar(): Char {
-        return source[pointer]
-    }
-
     private fun isNextChar(expected: Char):Boolean {
         if(isPointerOnEnd()) return false
         if(source[pointer+1]!=expected) return false
         return true
     }
+
+    private fun isExpectedChar(shift:Int, predicate: (char:Char) -> Boolean):Boolean{
+        if(pointer+shift >= source.length) return false
+        if(isPointerOnEnd()) return false
+        if(!predicate(source[pointer+shift])) return false
+        return true
+    }
+
+    private fun getPointedChar(): Char {
+        return source[pointer]
+    }
+
+    private fun getNextChar():Char{
+        return source[pointer+1]
+    }
+
+    private fun getSubstring():String{
+        return source.substring(entry,pointer)
+    }
+
 
     private fun incrementLine() = line++
 
@@ -140,5 +167,32 @@ class Scanner(private val source:String, private val interpreter: Interpreter) {
         }
     }
 
+    private fun addNumber(){
+        // Т.к. функция изначально вызвается на числе, то нужно передвинуть указатель.
+        while (!isPointerOnEnd()){
+            if(next.isDigit()) {
+                movePointer()
+                continue
+            }
 
+            if(isNextChar('.')){
+                if(isExpectedChar(2) { char -> char.isDigit() }){
+                    // Передвигаем указатель на точку.
+                    movePointer()
+                    continue
+                }
+                break
+            }
+            // Это значит мы дошли до конца файла.
+            break
+        }
+
+        val number = getSubstring().toFloatOrNull()
+        if(number == null)
+        {
+            Interpreter.error(interpreter, line, "Cannot parse number.")
+        }else{
+            addToken(NUMBER, number)
+        }
+    }
 }
